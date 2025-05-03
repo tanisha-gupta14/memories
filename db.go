a8e15c01d29a9477fbe4757e326bea5b74b60d4d
Tanisha <152022232+tanisha-gupta14@users.noreply.github.com>
2025-05-03 23:28:48 +0530

    fix bug

--- db.go
+++ db.go
@@ -11,7 +11,8 @@ var DB *sql.DB
 
 func InitDB() {
 	// This is your Railway public URL
-	dsn := "root:rIMXsXaJfXnQHgsLRiGqpAvTsgTAgMzU@tcp(ballast.proxy.rlwy.net:49782)/railway"
+	dsn := "root:rIMXsXaJfXnQHgsLRiGqpAvTsgTAgMzU@tcp(ballast.proxy.rlwy.net:49782)/railway?parseTime=true&tls=false"
+
 
 	var err error
 	DB, err = sql.Open("mysql", dsn)
