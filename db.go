cb91f1900c19eee9dbc240f28affc1d0828b10d9
Tanisha <152022232+tanisha-gupta14@users.noreply.github.com>
2025-05-03 22:54:30 +0530

    mysql migrated to railway

--- db.go
+++ db.go
@@ -3,15 +3,26 @@ package main
 import (
 	"database/sql"
 	"log"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var DB *sql.DB
 
 func InitDB() {
+	// This is your Railway public URL
+	dsn := "root:rIMXsXaJfXnQHgsLRiGqpAvTsgTAgMzU@tcp(ballast.proxy.rlwy.net:49782)/railway"
+
 	var err error
-	DB, err = sql.Open("mysql", "root:tan2005@tcp(127.0.0.1:3306)/memorydb")
+	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal("Failed to connect to DB:", err)
 	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("Failed to ping DB:", err)
+	}
+
+	log.Println("Connected to Railway DB!")
 }
